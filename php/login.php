b6c90f632edbc977d0432e6ae3bca6cd8f751045
GabrielV <gabrielvallejo2000@gmail.com>
2022-06-22 14:47:46 -0500

    Agregar commentarios

--- php/login.php
+++ php/login.php
@@ -1,20 +1,26 @@
 <?php
-    $data = array(
-        'secret' => "0x65844e4505D7683D505BC7E3bc13C0f283A467F8",
-        'response' => $_POST['capcha']
-    );
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "https://hcaptcha.com/siteverify");
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    $response = curl_exec($ch);
-    $responseData = json_decode($response);
-    if($responseData->success) {
-        // Si el captcha fue completado
-        echo 'Verificacion completada correctamente';
-    } else {
-        // En caso de no haber completado el captcha
-        echo 'Error de verificación de robot, por favor intente de nuevo.';
-    }
+
+// Definimos la secret key y recibimos la respuesta 
+$data = array(
+    'secret' => "0x65844e4505D7683D505BC7E3bc13C0f283A467F8",
+    'response' => $_POST['capcha']
+);
+
+// Hacemos el curl para verificar la respuesta
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "https://hcaptcha.com/siteverify");
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+$responseData = json_decode($response);
+
+if($responseData->success) {
+    // Si el captcha fue completado
+    echo 'Verificacion completada correctamente';
+} else {
+    // En caso de no haber completado el captcha
+    echo 'Error de verificación de robot, por favor intente de nuevo.';
+}
+
 ?>
